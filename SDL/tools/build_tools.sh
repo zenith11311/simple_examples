1746488ce86f35e81de775b510ec66d80f50b50e
Caelum <Caelum>
2017-08-18 16:31:49 +0900

    some error has occurred during SDL lib installation.... ah....

--- SDL/tools/build_tools.sh
+++ SDL/tools/build_tools.sh
@@ -9,6 +9,24 @@ fi
 SDL_SRC_HOME="${DEV_HOME}/tools/SDL2-2.0.5"
 #echo "$SDL_SRC_HOME"
 
+#  configure ; make all ; make install 중 아래와 같은 에러 발생....
+#  LTLINK build/libSDL2.la
+#  build/.libs/SDL_mirvideo.o: In function `MIR_InitDisplayFromOutput':
+#  /home/caelum/workspace/simple_workspace/SDL/tools/SDL2-2.0.5/src/video/mir/SDL_mirvideo.c:258: undefined reference to `mir_output_get_current_mode'
+#  collect2: error: ld returned 1 exit status
+#  Makefile:147: 'build/libSDL2.la' 타겟에 대한 명령이 실패했습니다
+#  make: *** [build/libSDL2.la] 오류 1
+#  /bin/bash build-scripts/updaterev.sh
+#     LTLINK build/libSDL2.la
+#     build/.libs/SDL_mirvideo.o: In function `MIR_InitDisplayFromOutput':
+#     /home/caelum/workspace/simple_workspace/SDL/tools/SDL2-2.0.5/src/video/mir/SDL_mirvideo.c:258: undefined reference to `mir_output_get_current_mode'
+#     collect2: error: ld returned 1 exit status
+#     Makefile:147: 'build/libSDL2.la' 타겟에 대한 명령이 실패했습니다
+#     make: *** [build/libSDL2.la] 오류 1
+
+
+
+
 function build_tool_sdl() {
     echo "building sdl...."
     tar xzvf ${DEV_HOME}/tools/SDL2-2.0.5.tar.gz -C ${DEV_HOME}/tools
